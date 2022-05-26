b3345e6ab7719f4a8d321b8ac65799b754362e81
Saravana Kumar <saravanak1999@gmail.com>
2022-05-27 03:33:36 +0530

    Part 2
    Added API calls to store and retrieve data

--- prop-management/src/app/app.component.ts
+++ prop-management/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogModelComponent } from './dialog-model/dialog-model.component';
-class Property{
+import {config} from '../config';
+class Property {
   name!: string;
-  desc!:string;
-  size!:number;
+  desc!: string;
+  size!: number;
 }
 
 @Component({
@@ -14,19 +15,48 @@ class Property{
   providers: [Property]
 })
 export class AppComponent {
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) { }
   title = 'prop-management';
   properties: Property[] = [];
+  MY_API_TOKEN=config.MY_API_TOKEN;
+  ngOnInit() {
+    fetch('https://api.airtable.com/v0/appPMyhVTxeM9HVw3/Property%20Details?api_key='+this.MY_API_TOKEN)
+      .then(response => response.json())
+      .then(data => {
+        let records = data.records;
+        if (records.length) {
+          for (let i = 0; i < records.length; i++) {
+            let temp: Property = { name: records[i].fields.name, desc: records[i].fields.desc, size: records[i].fields.size };
+            this.properties.push(temp);
+          }
+        }
+      });
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogModelComponent, {
       width: '250px',
-      disableClose:true,
+      disableClose: true,
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        this.properties.push(result);
+      if (result) {
+        let sampleBody={records:[{fields:result}]}
+        fetch('https://api.airtable.com/v0/appPMyhVTxeM9HVw3/Property%20Details', {
+          method: 'POST',
+          headers: {
+            'Authorization': 'Bearer '+this.MY_API_TOKEN,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(sampleBody),
+        })
+          .then(response => response.json())
+          .then(data => {
+            this.properties.push(result);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
       }
     });
   }
